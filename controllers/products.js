98afce5180ab2456798569cafe6c28c19b782660
Shreyas30 <jshreyas12@gmail.com>
2021-08-05 13:46:43 +0530

    Final Changes

--- controllers/products.js
+++ controllers/products.js
@@ -1,84 +1,181 @@
 import productModel from "../models/product.js";
+import stockUpdateModel from "../models/stockUpdate.js";
 
-
-
-
-export const getProducts = async (req,res) =>{
-    productModel.find(function(err, foundItems){
-        if(err){
-            res.status(404).json({message: err.message});
-        }else{
-            res.status(200).json({
-                message: "Success",
-                data: foundItems
-        })
-    }
-    })
-};
-export const addProduct = async (req,res) =>{
-    const newProduct = new productModel({
-        name : req.body.name,
-        category : req.body.category,
-        image : req.body.imageURL,
-        quantity : req.body.quantity,
-        price: req.body.price
-    })
-    try{
-        await newProduct.save()
-        res.status(200).json({message:"Added successfully"});
-    }
-   catch(err){
-       console.log(err)
-       res.status(404).json({message: err.message});
-   }
-};
-
-export const updateProduct = async (req,res) =>{
-
-    const updatedProduct = new productModel({
-        name : req.body.name,
-        category : req.body.category,
-        image : req.body.imageURL,
-        quantity : req.body.quantity,
-        price: req.body.price
-    })
-
-    productModel.updateOne({ _id: req.params.id}, updateProduct, function(err, res) {
-
-        if(err){
-            res.status(404).json({message: err.message});
-        }else{
-            res.status(200).json({
-                message: "Success",
-        })
-    }
-        // Updated at most one doc, `res.modifiedCount` contains the number
-        // of docs that MongoDB updated
+export const getProducts = (req, res) => {
+  productModel.find().select({image:0}).exec(function (err, foundItems) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json({
+        message: "Success",
+        data: foundItems,
       });
+    }
+  });
 };
 
-export const deleteProduct = (req,res) =>{
-    productModel.deleteOne({ _id: req.params.id }, function (err) {
-        if(err){
-            res.status(404).json({message: err.message});
-        }else{
-            res.status(200).json({
-                message: "Success",
-        })
-    }
-        // deleted item
+export const addProduct = async (req, res) => {
+  const newProduct = new productModel({
+    name: req.body.name,
+    category: req.body.category,
+    image: req.body.image,
+    quantity: Number(req.body.quantity),
+    quantityUnit: req.body.quantityUnit,
+    price: Number(req.body.price),
+  });
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ message: "Product Added successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  
+  const { name, category, image, price, quantity, quantityUnit } = req.body;
+  try {
+    const existingProduct = await productModel.findById(req.params.id);
+
+    const updatedProduct = ({
+      name: name ? name : existingProduct?.name,
+      category: category ? category : existingProduct.category,
+      image: image ? image : existingProduct.image,
+      price: price ? Number(price) : Number(existingProduct.price),
+      quantityUnit: quantityUnit ? quantityUnit : existingProduct.quantityUnit,
+      quantity: quantity ? Number(quantity) : Number( existingProduct.quantity),
+    });
+  
+    productModel.findByIdAndUpdate(
+      req.params.id ,
+      updatedProduct,
+      function (err, respose) {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(200).json({
+            message: "Product Updated Successfully",
+          });
+        }
       });
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
+
+  
 };
 
-export const searchProduct = (req,res) =>{
-    productModel.findOne({name: req.params.name}, function(err, foundItems){
-        if(err){
-            res.status(404).json({message: err.message});
-        }else{
-            res.status(200).json({
-                message: "Success",
-                data: foundItems
-        })
+export const deleteProduct = (req, res) => {
+  productModel.findByIdAndDelete({ _id: req.params.id }, function (err) {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      res.status(200).json({
+        message: "Product Deleted Successfully",
+      });
     }
-    })
+  });
 };
+
+export const searchProduct = (req, res) => {
+  const queryProduct = req.body;
+
+  productModel.find(queryProduct,{image:0}, function (err, foundItems) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      let message = "Success";
+      if (foundItems.length === 0) message = "No Match Found";
+
+      res.status(200).json({
+        message: message,
+        data: foundItems,
+      });
+    }
+  });
+};
+
+export const stockUpdate = (req, res) => {
+  stockUpdateModel.find()
+  .populate("product","name quantity")
+  .populate("addedBy","name username")
+  .exec(function (err, foundItems) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(200).json({
+        message: "Success",
+        data: foundItems,
+      });
+    }
+  });
+};
+
+export const buyProduct = async (req, res) => {
+  const { quantity } = req.body;
+  try {
+    const existingProduct = await productModel.findById(req.params.id);
+    if (existingProduct.quantity >= Number(quantity)) {
+      productModel.findByIdAndUpdate(
+       req.params.id ,
+        { quantity: existingProduct.quantity - Number(quantity) },
+        function (err, response) {
+          if (err) {
+            res.status(500).json({ error: err.message });
+          } else {
+            res.status(200).json({
+              message: "Succcess",
+            });
+          }
+        }
+      );
+    } else {
+      res.status(200).json({
+        message: "Not Enough Quantity",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+export const addStock = async (req, res) => {
+  
+  const { quantity } = req.body;
+  try {
+    const existingProduct = await productModel.findById(req.params.id);
+  
+    productModel.findByIdAndUpdate(
+      req.params.id ,
+      {quantity: existingProduct.quantity + Number(quantity)},
+      function (err, respose) {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: err.message });
+        } else {
+          stockUpdateModel.create(
+            { product: req.params.id, addedBy: req.userid, quantity: quantity },
+            (err1) => {
+              if (err1) {
+                console.error(err1);
+                res.status(500).json({ error: err.message });
+              } else {
+                res.status(200).json({
+                  message: "Product Updated Successfully",
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
+
+  
+};
+
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import {getProducts,addProduct,updateProduct,deleteProduct,searchProduct} from "../controllers/products.js";
+import {getProducts,addProduct,updateProduct,deleteProduct,searchProduct,stockUpdate,buyProduct,addStock} from "../controllers/products.js";
 const Router = express.Router();
 import Authenticate from "../middleware/auth.js";
+import AdminCheck from "../middleware/admin.js";
 
-Router.get('/', Authenticate,getProducts);
-Router.post('/',Authenticate, addProduct);
+Router.get('/',getProducts);
+Router.post('/',Authenticate,AdminCheck, addProduct);
+Router.patch('/buy/:id',Authenticate, buyProduct);
 Router.post("/search",searchProduct);
-Router.patch('/:id',Authenticate, updateProduct);
-Router.delete('/:id',Authenticate, deleteProduct);
+Router.get("/stockUpdate",Authenticate,AdminCheck,stockUpdate);
+Router.patch("/addStock/:id",Authenticate,AdminCheck,addStock);
+Router.patch('/:id',Authenticate,AdminCheck, updateProduct);
+Router.delete('/:id',Authenticate,AdminCheck, deleteProduct);
 
 export default Router;
